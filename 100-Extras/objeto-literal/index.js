bb4465e20c7ae345f654968d8a0a8beec018c9ab
César Aparicio <clark012389@hotmail.com>
2019-02-20 18:36:38 -0600

    Corrección de sintaxis en comentarios

--- 100-Extras/objeto-literal/index.js
+++ 100-Extras/objeto-literal/index.js
@@ -2,21 +2,18 @@
  * Declarando un objeto literal.
  * 
  * NOTA: Un objeto literal es aquel que declaramos de forma "literal", es decir que nosotros definimos sus valores
- * sin obtenerlos de algun recurso o API.
+ * sin obtenerlos de algún recurso o API.
 */
-
-
-
 
 const miObjeto = {
    propiedad1: 'Valor propiedad 1',
-   propiedad2: 'Otra cosa',
+   propiedad2: 'Valor propiedad 2',
    propiedad3: true,
    propiedad4: 23,
 
    metodo1: function () { 
-      // console.log('THIS LLAMADO DESDE METODO DENTRO DEL OBJETO LITERAL', this);
-      console.log(`Ejecutaste el metodo 1`);
+      // console.log('THIS LLAMADO DESDE método DENTRO DEL OBJETO LITERAL', this);
+      console.log(`Ejecutaste el método 1`);
    },
 
    metodo2: funcionMetodo2
@@ -25,27 +22,26 @@ const miObjeto = {
 
 function funcionMetodo2() { 
    // Se puede usar this para acceder a mis propiedades o metodos
-   this.propiedad2 = "Esto lo he modificado desde el metodo 2";
+   this.propiedad2 = "Esto lo he modificado desde el método 2";
 }
 
 
 //Veamos el valor de esas propiedades
-console.log(miObjeto.propiedad1);
-console.log(miObjeto.propiedad2);
+console.log(miObjeto.propiedad1); //Imprime: Valor propiedad 1
+console.log(miObjeto.propiedad2); //Imprime: Valor propiedad 2
 
-// Ejecutamos algun metodo
-miObjeto.metodo1();
-miObjeto.metodo2();
+// Ejecutamos algún método
+miObjeto.metodo1(); //Imprime: Ejecutaste el método 1
+miObjeto.metodo2(); //Modifica el valor de la propiedad 2
 
 // Creamos nueva proiedad del objeto 
 miObjeto.nuevaPropiedad5 = 'Soy la nueva propiedad5 creada Posteriormente';
 
-// Creamos un nuevo metodo
-
+// Creamos un nuevo método
 miObjeto.nuevoMetodo3 = function () {
    console.log("Soy en nuevo metodo3 creado Posteriormente")
 }
 
 
 // Veamos el contenido de todo el objeto
-console.info(miObjeto);
+console.info(miObjeto); //Imprime: el objeto y sus propiedades
