34ba9acb91ca51a68513353ac3042fc3a7f1b701
César Aparicio <clark012389@hotmail.com>
2019-03-19 22:27:13 -0600

    Finalizando sección de var

--- 01-repaso-var-scope/index.js
+++ 01-repaso-var-scope/index.js
@@ -1,34 +1,44 @@
-/** 
- * SCOPE: 
- * El escope(ámbito o alcance) de una variable es el contexto en el cual la variable existe, 
- * este nos indica en qué partes del programa podemos acceder a esa variable o si tenemos acceso a la misma. 
+/**
+ * SCOPE:
+ * El escope(ámbito o alcance) de una variable es el contexto en el cual la variable existe,
+ * este nos indica en qué partes del programa podemos acceder a esa variable o si tenemos acceso a la misma.
  * En JavaScript existen dos tipos de scope: LOCAL y GLOBAL.
-*/
+ */
 
-// Ejemplo 1 
+/** 
+ * Ejemplo 1 - Var en el contexto global
+ */
+
+console.warn('********** EJEMPLO 1 **********');
+
 for (var i = 0; i < 10; i++) {
    console.log(i);
 }
-
 
 console.log('Valor de i fuera del for -> ' + i); //Imprime: Valor de i fuera del for -> 10
 console.log(window.i); //Imprime 10
 
 /*
    Cuando nosotros usabamos una vairable 'var', esta dependia mucho de donde y con que cuidado la declarabamos,
-   en este caso var puede ser invocada fuera del for, porque al momento de declararla, esta se registró en el
+   en este caso 'var' puede ser invocada fuera del for, porque al momento de declararla, esta se registró en el
    objeto global 'window'.
    Nosotros pensariamos que el ultimo valor que tomo i fue 9, pero no es asi, ya que el ultimo valor
    que toma en el ciclo fue el ultimo incremento = 10.
    Al invocarla fuera con el console log, pensariamos que no se puede acceder al valor de i, sin enbargo
    en la consola nos regresa algo, esto debido a que la variable declarada como var i = 0, se alojo en el objeto window.
    Esto lo corroboramos al invocarla -i- dentro de window como un paramtro mas.
+
+   NOTA:
+   'Var' contamina el ambiente global, la unica forma de evitar esto es envolverlo en una funcion.
 */
 
-// Ejemplo 2
+/** 
+ * Ejemplo 2 - Var en 'Zona temporal de la muerte'
+ */
+console.warn('********** EJEMPLO 2 **********');
 
 if (false) {
-   var mensaje = 'Soy verdadero';
+   var mensaje = 'Soy falso';
 }
 
 console.log(mensaje); // Imprime: undefined
@@ -39,10 +49,94 @@ if (true) {
 
 console.log(mensaje); // Imprime: Soy verdadero
 
+/*
+   Un problema muy comun es que al estar viviendo en el contexto del global object,
+   si no se tiene cuidado, 'var' puede tener valores no esperados como 'undefinded', que no significa
+   que no exista, sino que aun no tiene un valor asignado.
+   Solo en el momento que entra a la condicion se asigna un valor.
+   Igual se observa que con 'var', se puede hacer una redeclaracion sin problema.
+*/
+
+
+/**
+ * EJEMPLO 3: Var y su referencia en las funciones
+ */
+console.warn('********** EJEMPLO 3 **********');
+
+var arregloFunciones = [];
+
+for (var i = 0; i < 10; i++) {
+   // console.log("Antes: " + i);
+
+   // Metodo 1
+   arregloFunciones.push(
+      function () {
+         console.info('Durante: ' + i);
+      }
+      // Aqui imprime 10 (10 veces), porque i se pasa por referencia
+   );
+
+   // Metodo 2
+   // arregloFunciones.push(
+   //    (function (valor) {
+   //       return function () {
+   //          console.info('Durante -> ' + valor);
+   //       };
+   //    })(i)
+   // );
+
+   console.log("Despues: " + i);
+}
+
+// 'i' mantiene la relacion con todas las funciones
+// Si modificamos i:
+// i = 100;
+// Imprime: Durante: 100 (10 veces)
+
+console.log('Valor de i fuera del for -> ' + i);
+
+arregloFunciones.forEach(function (valor, index) {
+   // console.log('Index ' + index);
+   valor();
+});
+
+/*
+   En el anterior ejemplo, se requiere construir un arreglo que contenga numeros incrementales del 0-9,
+   cada registro con una funcion que regrese el numero.
+   El primer metodo seria el mas comun, pero vemos que nos imprime 10 veces 10.
+   Esto se debe a que i esta declarada con un contexto global en el global object,
+   por lo que el ultimo valor que tome 'i', sera el valor que tome.
+
+   Para limitar el scope de 'var', se recurre a contenerlo dentro de una funcion, el metodo 2,
+   construye el arreglo en base a esa condicion. Es por eso que se muestran los numeros como se esperaban.
+
+   Si nosotros asignamos un nuevo valor a 'i' = 100 fuera del ciclo, nos mostrara 10 veces 100.
+   Esto porque toda variable 'var' declarada fuera de una funcion, se aloja en el global object, en este caso window,
+   que tiene un alcance global en nuestro entorno,
+*/
+
+/*
+   En resumen:
+   - 'var' al ser declarada se aloja en el global object, por consiguiente tiene un alcance global.
+   - 'var' puede ser limitada en su alcance(scope), envolviendola en una funcion
+   - 'var' puede ser definida/inicializada y actualizada
+   - 'var' puede ser reasignada despues de una inicializacion.
+   - 'var' puede ser hoisteada(levantada) en la parte superior de su scope(alcance).
+      Por lo que puede ser llamada antes de ser inicializada, no marca error porque es afectada pro el hoisting.
+ */
+
+ /*_________________________________________________________________________________________________*/
+
+ /**
+  * TODO: Pasar este parrafo en la seccion de 'let'
+ * Con 'let'  crea una nueva instancia de 'i', en cada iteracion, 
+ * por eso no se comparte esa referencia como lo hace 'var'.
+ * Por consiguiente nos ahorramos varias lineas de codigo
+ * y decrementamos la complejidad a una sola linea.
+ */
 
 /*
    NOTA: En un inicio con Javascript se penso en compartir los valores de la variable y que asi pudieran ser alojadas
    para su posterior uso o intercambio de informacion, pero el 'estado actual de javascript' es otro. 
    Tener una variable tipo var, contamina a nuestro ambito global, y no queremos eso.
 */
-
---
+++ ejercicios/index.js
@@ -0,0 +1,71 @@
+// (function () {
+//    console.info('Hola');
+// })();
+
+
+
+const PERSONA = {
+   nombre: 'Ramona',
+   edad: 20
+}
+
+PERSONA.apellido = 'Ramones';
+PERSONA.nombre = 'Ramones';
+// PERSONA = 1;  
+// Error: Porque se cambia la referencia
+
+// PERSONA = {} // Aun que se iguale aun obj vacio
+
+console.log(PERSONA);
+
+
+// hola();
+
+// function hola() {
+//    console.log(1);
+// }
+
+// console.log(paquito); // undefinded
+// var paquito = 'paquito';
+// console.log(paquito);
+
+
+
+console.log(1);
+console.log(2);
+
+setTimeout(function () { 
+   console.log(3);
+}, 0);
+
+
+console.log(4);
+
+
+// for (var i = 0; i < 4; i++) { 
+//    setTimeout(function () { 
+//       console.log(i);
+//    }, 0);
+// }
+
+// for (let i = 0; i < 4; i++) { 
+//    setTimeout(function () { 
+//       console.log(i);
+//    }, 0);
+// }
+
+
+
+const anonimo = {
+   nombre: 'Ramona',
+   apellido: 'Ramones',
+   edad: 17
+}
+
+console.log('El nombre completo es: ' + anonimo.nombre + anonimo.apellido);
+console.log(`El nombre completo es: ' ${anonimo.nombre} ${anonimo.apellido}`);
+
+
+console.log(`El nombre completo es: ' ${anonimo.nombre} ${anonimo.apellido} y es ${anonimo.edad > 18? 'mayor' : 'menor'} de edad`);
+
+
