92aa9769a1de25d80d26fef6941339ba9e234fd1
César Aparicio <clark012389@hotmail.com>
2020-10-14 23:20:03 -0500

    Agregando comentario adicional a el ejercicio

--- ejercicios/index.js
+++ ejercicios/index.js
@@ -2,22 +2,19 @@
 //    console.info('Hola');
 // })();
 
-
-
 const PERSONA = {
-   nombre: 'Ramona',
-   edad: 20
+  nombre: 'Ramona',
+  edad: 20,
 }
 
-PERSONA.apellido = 'Ramones';
-PERSONA.nombre = 'Ramones';
-// PERSONA = 1;  
-// Error: Porque se cambia la referencia
+PERSONA.apellido = 'Ramones'
+PERSONA.nombre = 'Ramones'
+// PERSONA = 1;
+// Error: Porque se cambia la referencia, de ser un objeto se quiere pasar a un número, y al ser 'const' no se puede reasignar el valor de la constante
 
 // PERSONA = {} // Aun que se iguale aun obj vacio
 
-console.log(PERSONA);
-
+console.log(PERSONA)
 
 // hola();
 
@@ -29,43 +26,38 @@ console.log(PERSONA);
 // var paquito = 'paquito';
 // console.log(paquito);
 
+console.log(1)
+console.log(2)
 
+setTimeout(function () {
+  console.log(3)
+}, 0)
 
-console.log(1);
-console.log(2);
+console.log(4)
 
-setTimeout(function () { 
-   console.log(3);
-}, 0);
-
-
-console.log(4);
-
-
-// for (var i = 0; i < 4; i++) { 
-//    setTimeout(function () { 
+// for (var i = 0; i < 4; i++) {
+//    setTimeout(function () {
 //       console.log(i);
 //    }, 0);
 // }
 
-// for (let i = 0; i < 4; i++) { 
-//    setTimeout(function () { 
+// for (let i = 0; i < 4; i++) {
+//    setTimeout(function () {
 //       console.log(i);
 //    }, 0);
 // }
-
-
 
 const anonimo = {
-   nombre: 'Ramona',
-   apellido: 'Ramones',
-   edad: 17
+  nombre: 'Ramona',
+  apellido: 'Ramones',
+  edad: 17,
 }
 
-console.log('El nombre completo es: ' + anonimo.nombre + anonimo.apellido);
-console.log(`El nombre completo es: ' ${anonimo.nombre} ${anonimo.apellido}`);
+console.log('El nombre completo es: ' + anonimo.nombre + anonimo.apellido)
+console.log(`El nombre completo es: ' ${anonimo.nombre} ${anonimo.apellido}`)
 
-
-console.log(`El nombre completo es: ' ${anonimo.nombre} ${anonimo.apellido} y es ${anonimo.edad > 18? 'mayor' : 'menor'} de edad`);
-
-
+console.log(
+  `El nombre completo es: ' ${anonimo.nombre} ${anonimo.apellido} y es ${
+    anonimo.edad > 18 ? 'mayor' : 'menor'
+  } de edad`
+)
